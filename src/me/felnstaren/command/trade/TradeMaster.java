d3aca08ecda6119a1329141b5e8c5511435646c2
RyanA3 <alsobrooks.ryan@gmail.com>
2020-02-15 23:02:33 -0500

    Add reload command, fine-tune sounds

--- src/me/felnstaren/command/trade/TradeMaster.java
+++ src/me/felnstaren/command/trade/TradeMaster.java
@@ -1,6 +1,7 @@
 package me.felnstaren.command.trade;
 
 import org.bukkit.command.CommandSender;
+import org.bukkit.entity.Player;
 
 import me.felnstaren.command.CommandStub;
 import me.felnstaren.command.MasterCommand;
@@ -8,6 +9,8 @@ import me.felnstaren.command.trade.accept.TradeAcceptSub;
 import me.felnstaren.command.trade.cancel.TradeCancelSub;
 import me.felnstaren.command.trade.deny.TradeDenySub;
 import me.felnstaren.command.trade.player.TradePlayerArgument;
+import me.felnstaren.command.trade.reload.TradeReloadSub;
+import me.felnstaren.config.ChatVar;
 import me.felnstaren.config.Language;
 import me.felnstaren.config.Options;
 
@@ -27,13 +30,29 @@ public class TradeMaster extends MasterCommand {
 		commands.add(new TradeAcceptSub());
 		commands.add(new TradeCancelSub());
 		commands.add(new TradeDenySub());
+		commands.add(new TradeReloadSub());
 		
 		arguments.add(new TradePlayerArgument());
 	}
 	
-	@Override public boolean handle(CommandSender sender, String[] args, int current) {
-		if(Options.use_commands) return forward(sender, args, current);
-		else return stub(sender, args);
+	@Override 
+	public boolean handle(CommandSender sender, String[] args, int current) {
+		if(!sender.hasPermission(this.permission) && Options.require_trade_permission) {
+			sender.sendMessage(Language.msg("err.no-permission", new ChatVar("[Permission]", permission)));
+			return true;
+		}
+		
+		if(!(sender instanceof Player)) {
+			sender.sendMessage(Language.msg("err.impossible-action"));
+			return true;
+		}
+		
+		if(Options.use_commands || (args.length > 0 && args[0].toLowerCase().equals("reload"))) {
+			if(!forward(sender, args, current)) 
+				sender.sendMessage(Language.msg("err.invalid-command"));
+		} else stub(sender, args);
+		
+		return true;
 	}
 
 }
